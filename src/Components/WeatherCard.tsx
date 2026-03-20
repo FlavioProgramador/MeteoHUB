8f6f42d71bb00926dcf3fe9fdeb439d7e3d78417
Flavio Costa <flavionegocios2004@gmail.com>
2026-03-20 13:15:52 -0300

    Estiliza componente 'WeatherCard' usando CSS Modules

--- src/Components/WeatherCard.tsx
+++ src/Components/WeatherCard.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import type { WeatherData } from "../Types/weather";
 import HeaderCard from "./HeaderCard";
 import MainCard from "./MainCard";
 import FooterCard from "./FooterCard";
+import styles from "./WeatherCard.module.css";
 
 interface WeatherCardProps {
   weather: WeatherData;
@@ -10,7 +10,7 @@ interface WeatherCardProps {
 
 const WeatherCard = ({ weather }: WeatherCardProps) => {
   return (
-    <div className="container-card">
+    <div className={styles.cardContainer}>
       <HeaderCard weather={weather} />
       <MainCard weather={weather} />
       <FooterCard />
