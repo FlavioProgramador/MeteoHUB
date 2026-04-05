0e819c7d7b6eb0dbfc7272b55a0f83a4e53184d1
Flavio Costa <flavionegocios2004@gmail.com>
2026-04-05 15:39:41 -0300

    feat(frontend): adiciona hook de título de página e atualiza estilos base e configuração principal do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-﻿import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import { Header } from "./Components/Header/Header";
+import { ErrorBoundary } from "./Components/ErrorBoundary/ErrorBoundary";
 import { PrivateRoute } from "./Components/PrivateRoute/PrivateRoute";
 import { Home } from "./Pages/Home/Home";
 import { Login } from "./Pages/Login/Login";
@@ -16,12 +17,28 @@ function App() {
           path="/"
           element={
             <PrivateRoute>
-              <Home />
+              <ErrorBoundary>
+                <Home />
+              </ErrorBoundary>
             </PrivateRoute>
           }
         />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={
+            <ErrorBoundary>
+              <Login />
+            </ErrorBoundary>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <ErrorBoundary>
+              <Register />
+            </ErrorBoundary>
+          }
+        />
       </Routes>
     </div>
   );
